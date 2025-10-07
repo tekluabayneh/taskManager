1270de3edb475520d30f380b9017d82b711e54a5
teklu abayneh <tekluabayneh@gmail.com>
2025-10-07 17:53:12 +0300

    feat: update Task handler with DB integration

--- handlers/task.go
+++ handlers/task.go
@@ -1,17 +1,42 @@
 package handlers
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+
+	db "github.com/tekluabayney/taskmanger/internal/db"
 )
 
-type Taskhandler struct{}
-
-func (T *Taskhandler) GetTask(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r)
-	fmt.Println("get task")
+type Taskhandler struct {
+	DB *db.Queries
 }
 
-func (T *Taskhandler) UpdateTask(w http.ResponseWriter, r *http.Request) {
+func (h *Taskhandler) GetTask(w http.ResponseWriter, r *http.Request) {
+	user, err := h.DB.GetUser(context.Background(), 2)
+	if err != nil {
+		fmt.Println("failed toget user")
+	}
+	fmt.Println("get task")
+	fmt.Println(user)
+}
+
+func (h *Taskhandler) UpdateTask(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("udpate task")
+	type newUser struct {
+		name string
+		age  int
+	}
+	nUser := db.CreateUserParams{
+		Name:  "one",
+		Email: "man@gmail.com",
+	}
+	user, err := h.DB.CreateUser(context.Background(), nUser)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(user)
+
 }
