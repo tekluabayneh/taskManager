e4e1c11da0b6268218282e8e09c6d2e5a14ab038
teklu abayneh <tekluabayneh@gmail.com>
2025-10-08 15:15:12 +0300

    feat: add task handlers for CRUD operations

--- handlers/task.go
+++ handlers/task.go
@@ -14,7 +14,7 @@ type Taskhandler struct {
 }
 
 func (h *Taskhandler) GetTask(w http.ResponseWriter, r *http.Request) {
-	user, err := h.DB.GetUser(context.Background(), 2)
+	user, err := h.DB.GetUser(context.Background(), 5)
 	if err != nil {
 		fmt.Println("failed toget user")
 	}
@@ -23,20 +23,29 @@ func (h *Taskhandler) GetTask(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Taskhandler) UpdateTask(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("udpate task")
 	type newUser struct {
 		name string
 		age  int
 	}
+
 	nUser := db.CreateUserParams{
 		Name:  "one",
 		Email: "man@gmail.com",
 	}
+
 	user, err := h.DB.CreateUser(context.Background(), nUser)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println(user)
+
+}
+
+func (h *Taskhandler) DeleteTask(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *Taskhandler) GetSingleTask(w http.ResponseWriter, r *http.Request) {
 
 }
