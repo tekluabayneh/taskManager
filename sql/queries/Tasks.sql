04fd9a5c71a0ad5055f41320fe351cd475eaf5e2
teklu abayneh <tekluabayneh@gmail.com>
2025-10-09 13:17:52 +0300

    modify SQL queries for tasks

--- sql/queries/Tasks.sql
+++ sql/queries/Tasks.sql
@@ -1,7 +1,7 @@
 -- name: GetTasks :many
 SELECT * FROM tasks;
 
--- name: createTask :one
+-- name: CreateTask :one
 INSERT INTO tasks(title, description, status, user_id)
 VALUES ($1, $2, $3, $4)
 RETURNING *;
