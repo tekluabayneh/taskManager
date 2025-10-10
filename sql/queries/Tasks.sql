500c1917e96a06cf85128709e82935f7dfcbb565
teklu abayneh <tekluabayneh@gmail.com>
2025-10-10 13:38:41 +0300

    Modify SQL queries related to tasks for better consistency

--- sql/queries/Tasks.sql
+++ sql/queries/Tasks.sql
@@ -5,3 +5,18 @@ SELECT * FROM tasks;
 INSERT INTO tasks(title, description, status, user_id)
 VALUES ($1, $2, $3, $4)
 RETURNING *;
+
+
+-- name: UpdateTask :one
+UPDATE tasks SET title = $1, description = $2, status = $3 WHERE id = $4
+RETURNING *;
+
+
+-- name: DeleteTask :one 
+DELETE  FROM tasks WHERE id = $1 
+RETURNING *;
+
+-- name: GetSingTask :one
+SELECT * from tasks WHERE id = $1;
+
+
